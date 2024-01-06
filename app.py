cbc707d80284136155efcc7700ec16143fa51dd8
awadih <abdoulahwadih@outlook.com>
2024-01-06 21:17:23 +0100

    add .

---
+++ app.py
@@ -0,0 +1,182 @@
+import json
+import pandas as pd
+import plotly.express as px  # type: ignore
+import plotly.utils  # type: ignore
+import os
+
+from utils.methods import *  # type: ignore
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_mail import Mail, Message  # type: ignore
+from dotenv import load_dotenv
+
+load_dotenv()
+api_ipgeo_key = os.getenv("API_IPGEO_KEY")
+api_google_key = os.getenv("API_GOOGLE_KEY")
+api_weatherapi_key = os.getenv("API_WEATHERAPI_KEY")
+mail_password = os.getenv("MAIL_PASSWORD")
+mail_username = os.getenv("MAIL_USERNAME")
+
+app = Flask(__name__, template_folder='templates')
+app.config["SECRET_KEY"] = os.getenv("API_SECRET")
+app.config['MAIL_SERVER'] = 'sandbox.smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = mail_username
+app.config['MAIL_PASSWORD'] = mail_password
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+mail = Mail(app)
+
+
+@app.route('/')
+def index():
+    """Renders the index page"""
+    return render_template('index.html', **locals())
+
+
+@app.route('/about')
+def about():
+    """Renders the about page"""
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    """Sends contact form and renders the index page"""
+    if request.method == 'POST':
+        msg = Message("Contact Form from DX-Dash",
+                      sender=request.form.get('name'),
+                      recipients=["dx-dash@mailtrap.io"])
+        msg.body = """
+            From: {} <{}>
+            To: DX-Dash <dx-dash@mailtrap.io>
+            Subject: New contact for DX-Dash
+            {}
+            """.format(request.form.get('name'), request.form.get('email'), request.form.get('message'))
+        mail.send(msg)
+    return render_template('index.html')
+
+
+@app.route('/home')
+def home():
+    """Renders the home page for current location at local time"""
+    # Get actual time
+    dt = datetime.datetime.now()
+    # Get current location as a dictionary (with ip address)
+    location_info = get_location(api_ipgeo_key)
+    # Reformat location to render title
+    location = f'{location_info["city"]} - {location_info["country"]}'
+    # Get full scope weather with weatherapi
+    weather = get_weather(api_weatherapi_key, location_info['city'])
+    # Get basic astrological info for current location at local time
+    sunrise, sunset = get_astro(weather)
+    # Get list for actual temperature, how does it feel like, air quality index and air quality description
+    quality = get_quality(weather)
+    # Get temperature forecast for next 3 days with weather icons
+    weather_days = forecast_days(dt, weather)
+    # Get weather forecast for next 24 hours with weather icons
+    weather_hours = forecast_hours(dt, weather)
+    # Get forecast for specific parameters
+    params = forecast_params(weather)
+    # Get alerts if any (modal as flag; if true, alerts are found)
+    modal, alerts = get_alerts(weather)
+    # Get embedded map link
+    map_link = get_map_link(api_google_key, location_info['city'])
+    # Get summary for city through teleport api
+    summary = get_summary(location_info['city'].lower())
+    return render_template("home.html", air_q_index=quality[2], air_q_desc=quality[3], summary=summary,
+                           temp=quality[0], feels=quality[1], map_link=map_link, sunrise=sunrise, sunset=sunset,
+                           location=location, weather_hours=weather_hours, weather_days=weather_days,
+                           params=params, alerts=[alerts[0], alerts[1], alerts[2]], modal=modal)
+
+
+@app.route('/explore', methods=['GET', 'POST'])
+def explore():
+    """Renders the explore page for weather in other locations around the globe"""
+    if request.method == 'POST' and request.form['search'] == 'search':
+        # Get actual time
+        dt = datetime.datetime.now()
+        # Get current location as a dictionary (with ip address)
+        location_info = get_location(api_ipgeo_key)
+        # Get current city of user
+        current_city = location_info['city']
+        # Get full scope weather with weatherapi
+        weather = get_weather(api_weatherapi_key, request.form['city'])
+        # Get basic astrological info for current location at local time
+        sunrise, sunset = get_astro(weather)
+        # Get list for actual temperature, how does it feel like, air quality index and air quality description
+        quality = get_quality(weather)
+        # Get actual time in user location
+        time_in_current_location = get_timezone(current_city).localize(dt)
+        # Get actual time in searched city
+        time_in_city = time_in_current_location.astimezone(get_timezone(request.form['city'].split(',')[0]))
+        # Get temperature forecast for next 3 days with weather icons
+        weather_days = forecast_days(time_in_city, weather)
+        # Get weather forecast for next 24 hours with weather icons
+        weather_hours = forecast_hours(dt, weather)
+        # Get forecast for specific parameters
+        params = forecast_params(weather)
+        # Get alerts if any (modal as flag; if true, alerts are found)
+        modal, alerts = get_alerts(weather)
+        # Get embedded map link
+        map_link, flag = get_src(api_google_key, request.form['city'].split(', ')[0].lower())
+        # Get summary for city through teleport api
+        summary = get_summary(request.form['city'].split(', ')[0].lower())
+        # ------------ PLOTS ------------ #
+        # Get weather for hours as dataframe
+        df = pd.DataFrame.from_dict(weather_hours)
+        # Transpose dataframe
+        df_transposed = df.T
+        # Change type to numerics
+        df_transposed['temperature'] = df_transposed['temperature'].apply(pd.to_numeric)
+        df_transposed['humidity'] = df_transposed['humidity'].apply(pd.to_numeric)
+        # Extract values for temperature and humidity
+        dc_temp = df_transposed['temperature']
+        dc_humidity = df_transposed['humidity']
+        # Create figure 1 with suitable layout update
+        fig1 = px.line(dc_temp, title='Temperature forecast in <i>degrees</i>')
+        fig1.update_traces(line_color='yellow')
+        fig1.update_layout(
+            paper_bgcolor='rgba(33, 37, 41, 0.3)',
+            plot_bgcolor='rgba(33, 37, 41, 0.05)',
+            font_color="white",
+            yaxis_title="",
+            xaxis_title="",
+            showlegend=False,
+            margin=dict(l=5, r=5, t=50, b=5),
+            font=dict(size=16, color="yellow"))
+        fig1.update_xaxes(mirror=True, ticks='outside', showline=True, gridcolor='lightgrey')
+        fig1.update_yaxes(mirror=True, ticks='outside', showline=True, gridcolor='lightgrey')
+        # Create figure 2 with suitable layout update
+        fig2 = px.line(dc_humidity, title='Humidity forecast in <i>percent</i>')
+        fig2.update_traces(line_color='darkturquoise')
+        fig2.update_layout(
+            paper_bgcolor='rgba(33, 37, 41, 0.3)',
+            plot_bgcolor='rgba(33, 37, 41, 0.05)',
+            font_color="white",
+            yaxis_title="",
+            xaxis_title="",
+            showlegend=False,
+            margin=dict(l=5, r=5, t=50, b=5),
+            font=dict(size=16, color="darkturquoise"))
+        fig2.update_xaxes(mirror=True, ticks='outside', showline=True, gridcolor='lightgrey')
+        fig2.update_yaxes(mirror=True, ticks='outside', showline=True, gridcolor='lightgrey')
+        # Create Graph Json objects to render
+        graph1json = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+        graph2json = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+        return render_template('explore.html', air_q_index=quality[2], air_q_desc=quality[3], temp=quality[0],
+                               feels=quality[1], sunrise=sunrise, sunset=sunset, summary=summary,
+                               location=request.form['city'], flag=flag,
+                               map_link=map_link, weather_hours=weather_hours, weather_days=weather_days,
+                               params=params, alerts=[alerts[0], alerts[1], alerts[2]], modal=modal,
+                               graph1json=graph1json,
+                               graph2json=graph2json)
+    return render_template('explore.html')
+
+
+if __name__ == '__main__':
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT, debug=True)
---
+++ utils/methods.py
@@ -0,0 +1,272 @@
+import datetime
+import requests
+
+from typing import Any, Dict, List, Union, Callable
+from geopy.geocoders import Nominatim  # type: ignore
+from pytz import timezone
+from timezonefinder import TimezoneFinder
+
+
+def get_days(time: datetime.datetime) -> tuple[str, str]:
+    """As the weather API allows retrieving for free only 3 days forecast, gets the next 3 days of weekdays
+
+    Args:
+        time: a datetime.datetime for actual time
+
+    Returns:
+        tuple giving tomorrow's and after tomorrow's names
+    """
+    weekday = time.weekday()
+    WEEKDAYS = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']
+    if weekday <= 4:
+        return WEEKDAYS[weekday + 1], WEEKDAYS[weekday + 2]
+    elif weekday == 5:
+        return WEEKDAYS[6], WEEKDAYS[0]
+    else:
+        return WEEKDAYS[0], WEEKDAYS[1]
+
+
+def get_location(ipgeo_key: str) -> dict[str, str]:
+    """Get the user location using the ip address through the IPGeolocation API
+
+    Args:
+        ipgeo_key: string for IPGeolocation API key
+
+    Returns:
+        dictionary for 'ip' address, 'city' and 'country' of the user
+    """
+    try:
+        response_ip = requests.get('https://api64.ipify.org?format=json')
+        response_ip.raise_for_status()
+        ip_address = response_ip.json()["ip"]
+        try:
+            response_location = requests.get(
+                f'https://api.ipgeolocation.io/ipgeo?apiKey={ipgeo_key}&ip={ip_address}&fields=city,country_name')
+            response_location.raise_for_status()
+            response = response_location.json()
+            location_data = {
+                "ip": str(response.get("ip")),
+                "city": str(response.get("city")),
+                "country": str(response.get("country_name"))
+            }
+            return location_data
+        except requests.exceptions.RequestException as e:
+            return {}
+        finally:
+            pass
+    except requests.exceptions.RequestException as e:
+        return {}
+
+
+def get_weather(weatherapi_key: str, city: str) -> dict[str, Any]:
+    """Gets the weatherapi response as json - names 'weather' in the code
+
+    Args:
+        weatherapi_key: Weatherapi API Key as string
+        city: urban location as string
+
+    Returns:
+        Get response as json (Dict of str -> Any)
+    """
+    # Weatherapi API
+    try:
+        response = requests.get(
+            f'https://api.weatherapi.com/v1/forecast.json?key={weatherapi_key}&q={city}&days=3&alerts=yes&aqi=yes')
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        return {}
+    finally:
+        pass
+
+
+def forecast_days(time: datetime.datetime, weather: Dict[str, Any]) -> Dict[str, Dict[str, str]]:
+    """Forecast temperatures and retrieve weather icons for 3 days
+
+    Args:
+        time: a datetime.datetime for actual time
+        weather: full weather data as json (dictionary)
+
+    Returns:
+        a dictionary for 'next 3' days, 'weather icon' and 'temperature forecast'
+    """
+    tomorrow, after_tomorrow = get_days(time)
+    return {'TODAY': {'weather_icon': weather['forecast']['forecastday'][0]['day']['condition']['icon'],
+                      'temperature': str(weather['forecast']['forecastday'][0]['day']['avgtemp_c']) + '°'},
+            tomorrow: {'weather_icon': weather['forecast']['forecastday'][1]['day']['condition']['icon'],
+                       'temperature': str(weather['forecast']['forecastday'][1]['day']['avgtemp_c']) + '°'},
+            after_tomorrow: {'weather_icon': weather['forecast']['forecastday'][2]['day']['condition']['icon'],
+                             'temperature': str(weather['forecast']['forecastday'][2]['day']['avgtemp_c']) + '°'}}
+
+
+def forecast_params(weather: Dict[str, Any]) -> Dict[str, str]:
+    """Give current humidity, pressure and wind speed
+
+    Args:
+        weather: full weather data as json (dictionary)
+
+    Returns:
+        dictionary for humidity, pressure and wind speed with corresponding units
+    """
+    humidity = weather["current"]["humidity"]
+    pressure = weather["current"]["pressure_mb"]
+    wind_speed = weather["current"]["wind_kph"]
+    return {'HUMIDITY': f'{humidity}%', 'PRESSURE': f'{pressure} hPa', 'WIND SPEED': f'{wind_speed} kmph'}
+
+
+def forecast_hours(time: datetime.datetime, weather: Dict[str, Any]) -> Dict[str, Dict[str, str]]:
+    """Get the weather forecast for next 24 hours
+
+    Args:
+        time: a datetime.datetime for actual time
+        weather: full weather data as json (dictionary)
+
+    Returns:
+        formatted dictionary with weather forecast as well as corresponding icons
+    """
+    now = datetime.time(time.hour)
+    formatted_weather = {'NOW': {
+        'weather_icon': weather['forecast']['forecastday'][0]['hour'][int(now.strftime('%H'))]['condition']['icon'],
+        'temperature': str(weather['forecast']['forecastday'][0]['hour'][int(now.strftime('%H'))]['temp_c']),
+        'humidity': str(weather['forecast']['forecastday'][0]['hour'][int(now.strftime('%H'))]['humidity']),
+        'wind_speed': str(weather['forecast']['forecastday'][0]['hour'][int(now.strftime('%H'))]['wind_kph'])
+    }
+    }
+    i = int(now.strftime('%H')) + 1
+    counting = 0
+    j = 0
+    while i <= 23:
+        time = time + datetime.timedelta(hours=1)
+        formatted_weather[time.strftime('%I').lstrip('0') + " " + time.strftime('%p').upper()] = {
+            'weather_icon': weather['forecast']['forecastday'][j]['hour'][i]['condition']['icon'],
+            'temperature': str(weather['forecast']['forecastday'][j]['hour'][i]['temp_c']),
+            'humidity': str(weather['forecast']['forecastday'][j]['hour'][i]['humidity']),
+            'wind_speed': str(weather['forecast']['forecastday'][j]['hour'][i]['wind_kph'])
+        }
+        if i + 1 == 24:
+            i = 0
+            j += 1
+        i += 1
+        counting += 1
+        if counting == 23:
+            break
+    return formatted_weather
+
+
+def get_astro(weather: Dict[str, Any]) -> tuple[str, str]:
+    """Get sunrise and sunset using weather response
+
+    Args:
+        weather: full weather data as json (dictionary)
+
+    Returns:
+        tuple with sunrise and sunrise for current location at local time
+    """
+    return weather["forecast"]["forecastday"][0]["astro"]["sunrise"], weather["forecast"]["forecastday"][0]["astro"][
+        "sunset"]
+
+
+def get_alerts(weather: Dict[str, Any]) -> tuple[bool, List[str]]:
+    """Get alerts for location at current time if any
+
+    Args:
+        weather: full weather data as json (dictionary)
+
+    Returns:
+        tuple with flag if alerts are found and alert headline, event and description
+    """
+    if weather["alerts"]["alert"]:
+        return True, [weather["alerts"]["alert"][0]['headline'], weather["alerts"]["alert"][0]['event'], \
+                      weather["alerts"]["alert"][0]['desc']]
+    return False, ['', '', '']
+
+
+def get_src(googleapi_key: str, city: str) -> tuple[str, bool]:
+    """Get source link for image or google map using teleport api / Google Maps api
+
+    Args:
+        googleapi_key: Google Maps API Key as string
+        city: location as string
+
+    Returns:
+        a string link to location photo with teleport api, if it's been found, a Google map source link otherwise.
+    """
+    # TELEPORT API
+    try:
+        city = city.replace(" ", "-")
+        response = requests.get(f'https://api.teleport.org/api/urban_areas/slug:{city}/images/')
+        response.raise_for_status()
+        if response.status_code != 204:
+            return response.json()['photos'][0]['image']['mobile'], True
+        else:
+            # Google Maps API
+            return get_map_link(googleapi_key, city), False
+    except Exception as e:
+        print(e)
+        # Google Maps API
+        return get_map_link(googleapi_key, city), False
+
+
+def get_timezone(city):  # type: ignore
+    """Get timezone for given location 'place'
+
+    Args:
+        city: string for current location
+
+    Returns:
+        timezone for the given location
+    """
+    geolocator = Nominatim(user_agent="app")
+    place_details = geolocator.geocode(city)
+    obj = TimezoneFinder()
+    time_zone = timezone(obj.timezone_at(lng=place_details[1][1], lat=place_details[1][0]))  # type: ignore
+    return time_zone
+
+
+def get_summary(city: str) -> str:
+    """Get a summary about the given city through Teleport API
+
+    Args:
+        city: string for given location
+
+    Returns:
+        A short summary about the given urban location
+    """
+    try:
+        city = city.replace(" ", "-")
+        response = requests.get(f'https://api.teleport.org/api/urban_areas/slug:{city}/scores/')
+        response.raise_for_status()
+        return response.json()['summary']
+    except Exception as e:
+        print(e)
+        return ''
+
+
+def get_quality(weather: Dict[str, Any]) -> List[str]:
+    """Get actual temperature, how does it feel like, air quality index a corresponding air quality description
+
+    Args:
+        weather: full weather data as json (dictionary)
+
+    Returns:
+        list with actual temperature, how does it feel like, air quality index a corresponding air quality description
+    """
+    AIR_Q_DESC = {1: 'Good', 2: 'Moderate', 3: 'Unhealthy for sensitive group', 4: 'Unhealthy', 5: 'Very Unhealthy',
+                  6: 'Hazardous'}
+    temperature = str(weather["current"]["temp_c"]) + "°"
+    feels_like = str(weather["current"]["feelslike_c"]) + "°"
+    air_q_index = weather["current"]["air_quality"]["us-epa-index"]
+    return [temperature, feels_like, air_q_index, AIR_Q_DESC[air_q_index]]
+
+
+def get_map_link(googleapi_key, city: str) -> str:
+    """Get google map link for city using the Google Maps API Key
+
+    Args:
+        googleapi_key: Google Maps api key as string
+        city: string for urban location
+
+    Returns:
+        source link to embedded map
+    """
+    return f'https://www.google.com/maps/embed/v1/place?key={googleapi_key}&q={city}&zoom=12'
